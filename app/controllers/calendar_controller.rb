e9b4a909ce0e8ee0f18284db5190135a6eeb9c96
timothyf <timothyf@gmail.com>
2010-01-23 21:12:34 -0500

    updates

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,2 +1,32 @@
 class CalendarController < ApplicationController
+  
+  def index
+    
+  end
+  
+  
+  def today
+    date = Time.now
+    # redirect to scoreboard passing today's date as params
+    redirect_to({:controller=>'Scoreboard', :action => "index", :year=>date.year, :month=>date.month, :date=>date.day})
+  end
+  
+  
+  def prev_day
+    @year = GamedayUtil.convert_digit_to_string(params[:year].to_i)
+    @month = GamedayUtil.convert_digit_to_string(params[:month].to_i)
+    @date = GamedayUtil.convert_digit_to_string(params[:date].to_i)
+    current_date = Time.local(@year, @month, @date)
+    # redirect to scoreboard passing prev day as params
+  end
+  
+  
+  def next_day
+    @year = GamedayUtil.convert_digit_to_string(params[:year].to_i)
+    @month = GamedayUtil.convert_digit_to_string(params[:month].to_i)
+    @date = GamedayUtil.convert_digit_to_string(params[:date].to_i)
+    current_date = Time.local(@year, @month, @date)
+    # redirect to scoreboard passing next day as params
+  end
+  
 end
--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,2 +1,14 @@
+require 'scoreboard'
+
 class ScoreboardController < ApplicationController
+  
+  def index
+    @year = GamedayUtil.convert_digit_to_string(params[:year].to_i)
+    @month = GamedayUtil.convert_digit_to_string(params[:month].to_i)
+    @date = GamedayUtil.convert_digit_to_string(params[:date].to_i)
+    sb = Scoreboard.new
+    sb.load_for_date(@year, @month, @date)
+    @games = sb.games
+  end
+  
 end
